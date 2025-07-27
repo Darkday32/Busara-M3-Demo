45847a57109f1598feb3bf5c4d9d870e1bedd5f8
Darkday32 <57805082+Darkday32@users.noreply.github.com>
2025-07-27 14:37:20 +0300

    Update __init__.py
    Signed-off-by: Darkday32 <57805082+Darkday32@users.noreply.github.com>

--- m3/demo/experts/__init__.py
+++ m3/demo/experts/__init__.py
@@ -2,13 +2,13 @@ from .base_expert import BaseExpert
 from .expert_monai_brats import ExpertBrats
 from .expert_monai_vista3d import ExpertVista3D
 from .expert_torchxrayvision import TorchXRayVisionExpert
-from .expert_tb import ExpertTB
+from .expert_tb import TBExpert
 
 __all__ = [
     "BaseExpert",
     "ExpertBrats",
     "ExpertVista3D",
     "TorchXRayVisionExpert",
-    "ExpertTB",
+    "TBExpert",
 ]
 
